6d083811e0b89d39715191ed5087cc6d85379ed6
AdamG-L <adamglicklynch1@gmail.com>
2025-01-30 14:42:32 -0500

    post req. generating notes

--- part3/index.js
+++ part3/index.js
@@ -41,11 +41,20 @@ app.get('/api/notes/:id', (request, response) => {
 })
 
 app.post('/api/notes', (request, response)=>{
-    const maxId = notes.length > 0 
-    // ... converts list to ind. params
-    ? Math.max(...notes.map(n => Number(n.id))) : 0
-    const note = request.body
-    note.id = String(maxId+1)
+    const body = request.body
+    if(!body.content){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    const note = {
+        content: body.content,
+        important: !!body.important,
+        id: generateId()
+    }
+
+    notes = notes.concat(note)
+
     response.json(note)
 })
 
@@ -59,3 +68,11 @@ const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+// Helper functions
+const generateId = () => {
+    const maxId = notes.length > 0 
+    // ... converts list to ind. params
+    ? Math.max(...notes.map(n => Number(n.id))) : 0
+    return String(maxId + 1)
+}
