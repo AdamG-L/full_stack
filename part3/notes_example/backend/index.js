992bbb0236497fe6186b47b4029e516f3001535f
AdamG-L <adamglicklynch1@gmail.com>
2025-02-07 14:55:07 -0500

    Implement get requests through mongodb

--- part3/notes_example/backend/index.js
+++ part3/notes_example/backend/index.js
@@ -48,14 +48,9 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-    if(note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    }
-    else{
-        response.status(404).end()
-    }
+    })
 })
 
 app.post('/api/notes', (request, response)=>{
@@ -65,15 +60,13 @@ app.post('/api/notes', (request, response)=>{
             error: 'content missing'
         })
     }
-    const note = {
+    const note = new Note({
         content: body.content,
         important: !!body.important,
-        id: generateId()
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
+    })
+    note.save().then(savedNote => {
+        response.json(savedNote)
+    })
 })
 
 app.delete('/api/notes/:id', (request, response)=> {
