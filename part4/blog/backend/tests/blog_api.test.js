fae0a837f4820d3e949576ff7e6b0f6e52727cf6
AdamG-L <adamglicklynch1@gmail.com>
2025-02-20 21:47:12 -0500

    refactor logging and add 'get' test

--- part4/blog/backend/tests/blog_api.test.js
+++ part4/blog/backend/tests/blog_api.test.js
@@ -1,5 +1,24 @@
-const { test, after, beforeEach } = require('node:test')
+const { test, after, describe, beforeEach } = require('node:test')
+const { initialBlogs, getBlogs } = require('./testHelper')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const Blog = require('../models/blog')
 const app = require('../app')
+const api = supertest(app)
+
+describe('Backend API Tests:', () => {
+
+    beforeEach(async () => {
+        await Blog.deleteMany({})
+        await Promise.all(initialBlogs.map(b => new Blog(b).save()))
+    })
+    after(async () => {
+        await mongoose.connection.close()
+    })
+
+    test('get request returns 2 blogs', async () => {
+        const response = await api.get('/api/blogs')
+        assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+})
