277740568a14fd7949c3545efe4dc36834936ede
AdamG-L <adamglicklynch1@gmail.com>
2025-02-20 21:54:00 -0500

    refactor blogs 'get'

--- part4/blog/backend/controllers/blogs.js
+++ part4/blog/backend/controllers/blogs.js
@@ -1,17 +1,16 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response, next) =>{
-    Blog.find({})
-    .then(blogs => response.json(blogs))
-    .catch(err => next(err))
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
 blogsRouter.post('/', (request, response, next) => {
     blog = new Blog(request.body)
     blog.save()
-    .then(savedBlog => response.status(201).json(savedBlog))
-    .catch(err => next(err))
+        .then(savedBlog => response.status(201).json(savedBlog))
+        .catch(err => next(err))
 })
 
 module.exports = blogsRouter
