ae4dc0c2f14718aaf91902748b1a4b98ce690c93
AdamG-L <adamglicklynch1@gmail.com>
2025-03-01 21:58:50 -0500

    Update post request for blog and user to display associated values

--- part4/blog/backend/controllers/blogs.js
+++ part4/blog/backend/controllers/blogs.js
@@ -1,14 +1,25 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const { jwtMiddleware } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
+    .populate('user', {username: 1, name: 1})
     response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-    blog = new Blog(request.body)
+blogsRouter.post('/', jwtMiddleware, async(request, response) => {
+    // Get user.id from token
+    const user = await User.findById(request.user.id)
+    const blog = new Blog({
+        ...request.body,
+        user: user._id
+    })
     savedBlog = await blog.save()
+    // Update associated users blog array
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
 })
 
