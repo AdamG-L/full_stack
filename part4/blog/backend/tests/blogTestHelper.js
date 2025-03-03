37d23bdfde1240932708cdac9adfde9e1d1a8fe7
AdamG-L <adamglicklynch1@gmail.com>
2025-03-03 18:08:56 -0500

    Remove Unused Code / change tests to sequential

--- part4/blog/backend/tests/blogTestHelper.js
+++ part4/blog/backend/tests/blogTestHelper.js
@@ -33,28 +33,6 @@ const getBlogs = async () => {
     return blogs.map(b => b.toJSON())
 }
 
-const initializeUsers = async () => {
-    const saltRounds = 10
-    await Promise.all(initialUsers.map(async (u) => {
-        const passwordHash = await bcrypt.hash(u.password, saltRounds)
-        await new User({...u,
-            passwordHash
-        }).save()
-    }))
-}
-
-const initializeBlogs = async (username) => {
-    // Manually add associated user
-    const user = await User.findOne({ username })
-    const userId = user._id.toString()
-    await Promise.all(initialBlogs.map(async (b) => {
-        const blog = await new Blog({ ...b, user: userId }).save()
-        // Add blogId to user
-        user.blogs = user.blogs.concat(blog._id)
-    }))
-    await user.save()
-}
-
 module.exports = {
-    initialBlogs, initialUsers, getBlogs, initializeUsers, initializeBlogs
+    initialBlogs, initialUsers, getBlogs
 }
