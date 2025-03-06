3a44ee1e1533312c5d986788852e01c485420a16
AdamG-L <adamglicklynch1@gmail.com>
2025-03-05 21:12:32 -0500

    Working Login Form for blog frontend

--- part5/blog/frontend/src/App.jsx
+++ part5/blog/frontend/src/App.jsx
@@ -1,11 +1,60 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import LoginForm from './components/LoginForm'
+import Notification from './components/Notification'
+import loginService from './services/login'
+import blogService from './services/blog'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+
+  const [errorMsg, setErrMsg] = useState("")
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  // Check for existing token
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
+      const loggedUser = JSON.parse(loggedUserJSON)
+      setUser(loggedUser)
+      blogService.setToken(loggedUser.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedUser = await loginService.login({
+        username, password
+      })
+      window.localStorage
+        .setItem('loggedUser', JSON.stringify(loggedUser))
+      blogService.setToken(loggedUser.token)
+      setUser(loggedUser)
+      setUsername('')
+      setPassword('')
+    } catch {
+      setErrMsg('Wrong credentials')
+      setTimeout(() => {
+        setErrMsg(null)
+      }, 5000)
+    }
+  }
+
 
   return (
     <>
-      HELLO WORLD
+      <Notification errorMsg={errorMsg} />
+      <h1>Blog App</h1>
+      {
+        user === null ?
+          <LoginForm username={username} password={password}
+            setUsername={setUsername} setPassword={setPassword}
+            handleLogin={handleLogin} />
+          :
+          <h3>Login Successful</h3>
+      }
     </>
   )
 }
