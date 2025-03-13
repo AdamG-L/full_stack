5aac0158f20a4239685c1db34acdd3a1e565443b
AdamG-L <adamglicklynch1@gmail.com>
2025-03-12 22:12:23 -0400

    Added simple prop-type test in Togglable

--- part5/blog/backend/controllers/blogs.js
+++ part5/blog/backend/controllers/blogs.js
@@ -5,11 +5,11 @@ const { jwtMiddleware } = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
-    .populate('user', {username: 1, name: 1})
+        .populate('user', { username: 1, name: 1 })
     response.status(200).json(blogs)
 })
 
-blogsRouter.post('/', jwtMiddleware, async(request, response) => {
+blogsRouter.post('/', jwtMiddleware, async (request, response) => {
     // Get user.id from token
     const user = await User.findById(request.user.id)
     const blog = new Blog({
@@ -28,13 +28,13 @@ blogsRouter.delete('/:id', jwtMiddleware, async (request, response) => {
     const userId = request.user.id
     const blog = await Blog.findById(blogId)
     const user = await User.findById(userId)
-    if(!blog || !user){
+    if (!blog || !user) {
         return response.status(204).end()
     }
-    if(blog.user.toString() !== userId){
+    if (blog.user.toString() !== userId) {
         return response.status(403).json({
             error: 'You can only delete blogs you own'
-          })
+        })
     }
     user.blogs = user.blogs.filter(b => b.toString() !== blogId)
     await user.save()
@@ -43,13 +43,13 @@ blogsRouter.delete('/:id', jwtMiddleware, async (request, response) => {
 })
 
 blogsRouter.put('/:id', jwtMiddleware, async (request, response) => {
-    const updatedBlog = 
-    await Blog.findByIdAndUpdate(request.params.id,
-        { $set: request.body }, {
+    const updatedBlog =
+        await Blog.findByIdAndUpdate(request.params.id,
+            { $set: request.body }, {
             new: true, runValidators: true,
             context: 'query'
         }
-    )
+        )
     response.status(200).json(updatedBlog)
 })
 
