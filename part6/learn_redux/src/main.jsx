349f57677d95390dd591c7ab97e6511ce562fcc3
AdamG-L <adamglicklynch1@gmail.com>
2025-03-21 16:11:56 -0400

    Unicafe App w/ redux

--- part6/learn_redux/src/main.jsx
+++ part6/learn_redux/src/main.jsx
@@ -41,4 +41,12 @@ const App = () => {
     </div>
   )
 }
-export default noteReducer;
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
