ccc95c5c1e0803b8d5638dd25b45baa5394249ae
AdamG-L <adamglicklynch1@gmail.com>
2025-03-24 15:17:34 -0400

    Rename learn_redux to note_app

--- part6/note_app/src/App.jsx
+++ part6/note_app/src/App.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
 import NoteForm from './components/NoteForm'
 import Notes from './components/Notes'
 import VisibilityFilter from './components/VisibilityFilter'
 
 
 const App = () => {
-  const filterSelected = (value) => {
-    console.log(value)
-  }
   return (
     <div>
       <NoteForm />
