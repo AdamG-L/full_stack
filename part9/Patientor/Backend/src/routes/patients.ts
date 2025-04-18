3c3fed95cd6d15afd753a56e048b0a84bb0eef1e
AdamG-L <adamglicklynch1@gmail.com>
2025-04-18 17:59:46 -0400

    Patientor Post Req. w/ TypeGuarding

--- part9/Patientor/Backend/src/routes/patients.ts
+++ part9/Patientor/Backend/src/routes/patients.ts
@@ -2,11 +2,26 @@ import express from 'express'
 import {Response} from 'express'
 import { PatientPublic } from '../types'
 import patientService from '../services/patientService'
+import parseNewPatient from '../utils'
 
 const router = express.Router()
 
 router.get('/', (_req, res: Response<PatientPublic[]>) => {
     res.send(patientService.getEntries())
+})
+
+router.post('/', (req, res) => {
+    try {
+        const newPatient = parseNewPatient(req.body)
+        const patient = patientService.addPatient(newPatient)
+        res.status(201).send(patient)
+      } catch (error: unknown) {
+        let errorMessage = 'Something went wrong.'
+        if (error instanceof Error) {
+          errorMessage += ' Error: ' + error.message
+        }
+        res.status(400).send(errorMessage)
+      }
 })
 
 export default router
--- part9/Patientor/Backend/src/services/patientService.ts
+++ part9/Patientor/Backend/src/services/patientService.ts
@@ -1,10 +1,21 @@
-import { PatientPublic } from "../types";
-import patients from "../../data/patients";
+import { NewPatient, Patient, PatientPublic } from "../types"
+import patients from "../../data/patients"
+import { v4 as uuid } from 'uuid'
 
 const getEntries = (): PatientPublic[] => {
-    return patients.map(({ssn: _ssn, ...publicData}) => publicData)
+    return patients.map(({ ssn: _ssn, ...publicData }) => publicData)
+}
+
+const addPatient = (patient: NewPatient): Patient => {
+    const addedPatient = {
+        id: uuid(),
+        ...patient
+    }
+    patients.push(addedPatient)
+    return addedPatient
 }
 
 export default {
-    getEntries
+    getEntries,
+    addPatient,
 }
