d5ee81117d33a0573f4ad1f0b5a7932ac98832b8
AdamG-L <adamglicklynch1@gmail.com>
2025-04-27 20:30:43 -0400

    Simply Functional Frontend w/ TS

--- part9/ts_notes/src/App.tsx
+++ part9/ts_notes/src/App.tsx
@@ -1,10 +1,41 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
+import { Note } from './types'
+import {createNote, getAllNotes} from './services/noteService'
 
 const App = () => {
-  const [newNote, setNewNote] = useState('');
-  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('')
+  const [notes, setNotes] = useState<Note[]>([
+    { id: '1', content: 'testing' }
+  ])
 
-  return null
+  useEffect(() => {
+    getAllNotes()
+    .then(data => setNotes(data))
+  }, [])
+
+  const noteCreation = async (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const noteToBeAdded = await createNote({content: newNote})
+    setNotes(notes.concat(noteToBeAdded))
+    setNewNote('')
+  }
+
+  return (
+    <div>
+       <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type='submit'>add</button>
+      </form>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id}>{note.content}</li>
+        )}
+      </ul>
+    </div>
+  )
 }
 
 export default App
