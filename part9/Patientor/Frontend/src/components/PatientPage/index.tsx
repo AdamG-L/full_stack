aeb9336fa916d1645ab45cefc8db6dd080b8951a
AdamG-L <adamglicklynch1@gmail.com>
2025-05-01 02:14:01 -0400

    Setup Patient Page based on Ids styled w/ MaterialUI

---
+++ part9/Patientor/Frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,50 @@
+import { Alert, Box, Card, CardContent, Typography,  } from "@mui/material"
+import MaleIcon from '@mui/icons-material/Male'
+import FemaleIcon from '@mui/icons-material/Female'
+import Person from '@mui/icons-material/Person'
+import { Gender, Patient } from "../../types"
+
+type Props = {
+    patient: Patient | null | undefined
+}
+const PatientPage = ({ patient }: Props) => {
+    if (!patient) {
+        return <Alert severity="error">Error: Patient could not be found</Alert>;
+    }
+    return (
+        <Box mt={4} display="flex" justifyContent="center">
+            <Card sx={{ minWidth: 300, maxWidth: 500 }}>
+                <CardContent>
+                    <Typography variant="h5" component="div" gutterBottom>
+                        {patient.name} {getGenderIcon(patient.gender)}
+                    </Typography>
+                    <Typography color="text.secondary">
+                        Date of Birth: {patient.dateOfBirth}
+                    </Typography>
+                    <Typography color="text.secondary">
+                        Gender: {patient.gender}
+                    </Typography>
+                    <Typography color="text.secondary">
+                        Occupation: {patient.occupation}
+                    </Typography>
+                    <Typography color="text.secondary">
+                        SSN: {patient.ssn}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+    )
+}
+
+const getGenderIcon = (gender: Gender) => {
+    switch (gender) {
+        case Gender.Male:
+          return <MaleIcon />
+        case Gender.Female:
+          return <FemaleIcon />
+        default:
+          return <Person />
+      }
+}
+
+export default PatientPage
