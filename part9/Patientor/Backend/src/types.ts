1fc706ef59102b6bb96dcb0cea7ed852a792b640
AdamG-L <adamglicklynch1@gmail.com>
2025-05-02 15:55:30 -0400

    Update typing on Entries

--- part9/Patientor/Backend/src/types.ts
+++ part9/Patientor/Backend/src/types.ts
@@ -1,13 +1,68 @@
 import {z} from 'zod'
+/******  Custom Union ******/
+type UnionOmit<T, K extends string | number | symbol> 
+= T extends unknown ? Omit<T, K> : never
+/*****************************************/
+
+/******  Diagnosis type definitions ******/
 export interface Diagnosis {
     code: string,
     name: string,
     latin?: string,
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface Entry {
+/*****************************************/
+
+
+/******  Entry type definitions ******/
+export interface BaseEntry {
+    id: string, 
+    description: string,
+    date: string,
+    specialist: string,
+    diagnosisCodes?: Array<Diagnosis['code']>,
 }
 
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3,
+}
+
+export interface SickLeave {
+    startDate: string,
+    endDate: string,
+}
+
+export interface Discharge {
+    date: string, 
+    criteria: string,
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck",
+    healthCheckRating: HealthCheckRating,
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare",
+    employerName: string,
+    sickLeave?: SickLeave,
+}
+
+export interface HospitalEntry extends BaseEntry {
+    type: "Hospital",
+    discharge: Discharge,
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry
+/*****************************************/
+
+
+/******  Patient type definitions ******/
 export interface Patient {
     id: string,
     name: string,
@@ -24,8 +79,8 @@ export enum Gender{
     Other = 'other'
 }
 
-export type PatientPublic = Omit<Patient, 'ssn' | 'entries'>
-export type NewPatient = Omit<Patient, 'id'>
+export type PatientPublic = UnionOmit<Patient, 'ssn' | 'entries'>
+export type NewPatient = UnionOmit<Patient, 'id'>
 
 export const NewPatientSchema = z.object({
     name: z.string(),
@@ -34,3 +89,5 @@ export const NewPatientSchema = z.object({
     gender: z.nativeEnum(Gender),
     occupation: z.string(),
 })
+/*****************************************/
+
